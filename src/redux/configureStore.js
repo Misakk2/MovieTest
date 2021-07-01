8b02cea87639d8c08981414758a3df3e4375c642
Michal Smriga <michal.smriga@gmail.com>
2021-07-01 10:28:58 +0200

    AddSagatoPoroject

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -15,7 +15,9 @@ const reducer = combineReducers({
 const store = createStore(
     reducer,
     {},
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
 );
 
 export default store;
+/*
+window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() */
---
+++ src/sagas/rootSaga.js
@@ -0,0 +1,5 @@
+function* rootSaga() {
+    yield console.log('Inside Root Saga.')
+}
+
+export default rootSaga;
